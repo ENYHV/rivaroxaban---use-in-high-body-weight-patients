6c07a6326936a4f79fa3f20299666fc20952ee29
Amy Zhou <133985675+ENYHV@users.noreply.github.com>
2023-09-11 14:22:23 -0400

    Add files via upload

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -10,6 +10,14 @@ function link4() {
     var someTabTriggerEl = document.querySelector('#additional_lit_tab'); var tab = new bootstrap.Tab(someTabTriggerEl); tab.show(); window.scrollTo(0,0);
 }
 
+function changeIcon(anchor) {
+    var icon = anchor.querySelector("i");
+    icon.classList.toggle('fa');
+    icon.classList.toggle('fa-times');
+
+    anchor.querySelector("span").textContent = icon.classList.contains('fa');
+}
+
 $(document).ready(function(){ 
     $(window).scroll(function(){ 
         if ($(this).scrollTop() > 100) { 
